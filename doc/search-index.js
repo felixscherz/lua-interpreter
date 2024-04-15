083e1a32c877adbfc77d770f3fc27a6f595ffeda
Felix Scherz <felixwscherz@gmail.com>
2024-04-15 21:10:24 +0200

    docs: experiment with rustdoc

---
+++ doc/search-index.js
@@ -0,0 +1,6 @@
+var searchIndex = new Map(JSON.parse('[\
+["lib",{"doc":"","t":"H","n":["lua"],"q":[[0,"lib"],[1,"std::fs"],[2,"std::io"]],"d":[""],"i":[0],"f":[[[1,2],3]],"c":[],"p":[[5,"File",1],[10,"Write",2],[1,"tuple"]],"b":[]}],\
+["lua_interpreter",{"doc":"","t":"H","n":["lua"],"q":[[0,"lua_interpreter"],[1,"std::fs"],[2,"std::io"]],"d":[""],"i":[0],"f":[[[1,2],3]],"c":[],"p":[[5,"File",1],[10,"Write",2],[1,"tuple"]],"b":[]}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
---
+++ doc/src-files.js
@@ -0,0 +1,5 @@
+var srcIndex = new Map(JSON.parse('[\
+["lib",["",[],["bytecode.rs","lexer.rs","lib.rs","parser.rs","value.rs","vm.rs"]]],\
+["lua_interpreter",["",[],["bytecode.rs","lexer.rs","lib.rs","parser.rs","value.rs","vm.rs"]]]\
+]'));
+createSrcSidebar();
