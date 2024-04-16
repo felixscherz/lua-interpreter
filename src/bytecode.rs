52d98e74e9d6b35ea8dbe28beb6c7af6d46c0d95
Felix Scherz <felixwscherz@gmail.com>
2024-04-16 21:28:16 +0200

    docs: add documentation to GetGlobal and LoadConst bytecodes

--- src/bytecode.rs
+++ src/bytecode.rs
@@ -1,6 +1,10 @@
 #[derive(Debug)]
 pub enum ByteCode {
+    /// GetGlobal(dst, src):
+    /// load global name from constants at src and load global into stack at dst
     GetGlobal(u8, u8),
+    /// LoadConst(dst, src):
+    /// load value from constants into stack at dst
     LoadConst(u8, u8),
     Call(u8, u8),
 }
