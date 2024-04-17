a889d2ada659b066124193a18f6985ff66822a8d
Felix Scherz <felixwscherz@gmail.com>
2024-04-17 17:47:49 +0200

    docs: work through how function call is implemented so far

--- src/parser.rs
+++ src/parser.rs
@@ -20,9 +20,15 @@ pub fn load(stream: &mut File) -> ParseProto {
         match lex.next() {
             Token::Name(name) => {
                 // `Name LiteralString` as function call
+                // Push function name to the constants
                 constants.push(Value::String(name));
+                // Push instructions to get function name from constants and push to stack at 0
                 byte_codes.push(ByteCode::GetGlobal(0, (constants.len() - 1) as u8));
 
+                // Parse the string argument, save it to the constants.
+                // Add instruction to load the argument from constants onto the stack on top of
+                // the function call, then push a call instruction which is supposed to use the
+                // function at index 0 and use index 1 as arguments
                 if let Token::String(s) = lex.next() {
                     constants.push(Value::String(s));
                     byte_codes.push(ByteCode::LoadConst(1, (constants.len() - 1) as u8));
